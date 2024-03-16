bb80ecd6315d7c9bbe244066a3e5f4dfb49c5b82
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-03-16 13:54:30 +0100

    commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,51 @@
-function gamePlayTime(name) {
-  const profile = {
-    userName: name.userName,
-    playTime: name.playTime,
+// function gamePlayTime(name) {
+//   const profile = {
+//     userName: name.userName,
+//     playTime: name.playTime,
+
+//     changeUsername(newName) {
+//       this.userName = newName;
+//     },
+
+//     updatePlayTime(hours) {
+//       this.playTime += hours;
+//     },
+
+//     getInfo() {
+//       return `${this.userName} has ${this.playTime} active hours!`;
+//     },
+//   };
+//   return profile;
+// }
+
+const profile = ({ userName, playTime }) => {
+  return {
+    username: userName,
+    playtime: playTime,
 
     changeUsername(newName) {
-      this.userName = newName;
+      this.username = newName;
     },
 
     updatePlayTime(hours) {
-      this.playTime += hours;
+      this.playtime += hours;
     },
 
     getInfo() {
-      return `${this.userName} has ${this.playTime} active hours!`;
+      return `${this.username} has ${this.playtime} active hours!`;
     },
   };
-  return profile;
-}
+};
 
-const userProfile = gamePlayTime({ userName: "Jacob", playTime: 300 });
-
-console.log(userProfile.getInfo()); // "Jacob has 300 active hours!"
+// Example usage:
+const userProfile = profile({ userName: "Jacob", playTime: 300 });
+console.log(userProfile.getInfo()); // Output: Jacob has 300 active hours!
 
 userProfile.changeUsername("Marco");
-console.log(userProfile.getInfo()); // "Marco has 300 active hours!"
+console.log(userProfile.getInfo()); // Output: Marco has 300 active hours!
 
 userProfile.updatePlayTime(20);
-console.log(userProfile.getInfo()); // "Marco has 320 active hours!"
+console.log(userProfile.getInfo()); // Output: Marco has 320 active hours!
 
 // Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
 // Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
